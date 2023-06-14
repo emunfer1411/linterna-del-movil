b5922c29dc1b8946b600a5ce72dc4380fec02640
emunfer1411 <130557119+emunfer1411@users.noreply.github.com>
2023-06-14 11:51:27 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 let velocidad = 30
 basic.forever(function () {
     if (input.lightLevel() < 125) {
-        maqueen.motorStop(maqueen.Motors.M2)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, velocidad)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, velocidad)
     } else {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
     }
